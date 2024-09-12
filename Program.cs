669cc6dc21254fd2400c54d64a5bd5bed94b0db6
RylanStevens <stevens.rylan@icloud.com>
2024-09-11 22:52:39 -0600

    The tests said the passed so I think it worked

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@ public class Program
 {
     static void Main(string[] args)
 {
-    Debug.Assert(Enumerable.SequenceEqual(CombinedSortedArrays(new int[]{1, 3, 5}, new int []{-5, 3, 6, 7}), new int[]{-5, 1, 3, 3, 5, 6, 7}));
+    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(CombineSortedArrays(new int[]{1, 3, 5}, new int []{-5, 3, 6, 7}), new int[]{-5, 1, 3, 3, 5, 6, 7}));
+    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(CombineSortedArrays(new int[]{-5, 2, 5, 8, 10}, new int[]{1, 2, 5}), new int[]{-5, 1, 2, 2, 5, 5, 8, 10}));
+    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(SortViaMergesort(new int[]{6, 1, -5, 3, 5, 3, 7}), new int[]{-5, 1, 3, 3, 5, 6, 7}));
+    System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(SortViaMergesort(new int[]{1, 10, -5, 2, 5, 2, 5, 8}), new int[]{-5, 1, 2, 2, 5, 5, 8, 10}));
     Console.WriteLine("pass");
 }
-static int[] CombinedSortedArrays(int[] a, int[] b)
+static int[] CombineSortedArrays(int[] a, int[] b)
 {
     int[] combined = new int[a.Length + b.Length];
     int aIndex = 0, bIndex = 0, combinedIndex = 0;
@@ -31,5 +34,18 @@ static int[] CombinedSortedArrays(int[] a, int[] b)
         combined[combinedIndex++] = b[bIndex++];
     }
     return combined;
+}
+
+public static int[] SortViaMergesort(int[] values)
+{
+    if (values.Length < 2)
+    {
+        return (int[])values.Clone();
+    }
+    int middle = values.Length / 2;
+    int[] firstHalf = values[0..middle];
+    int[] secondHalf = values[middle..values.Length];
+    return CombineSortedArrays(SortViaMergesort(firstHalf), SortViaMergesort(secondHalf));
+
 }
 }
--- obj/Debug/net8.0/lab-2-merge-sort-RylanStevens.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-2-merge-sort-RylanStevens.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-2-merge-sort-RylanStevens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d130e6da35ad252f9e81123da0178266f6741c96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70558d8dc272860475387a390ae7229fbce1e24d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-2-merge-sort-RylanStevens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-2-merge-sort-RylanStevens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
